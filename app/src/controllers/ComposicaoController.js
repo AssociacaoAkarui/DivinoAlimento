80482049e7b998dc9c407c8b88bb53b3b7ba9bb7
Carmen Freitas <carmen.freitas@gmail.com>
2025-05-22 11:49:57 -0300

    Akarui/DivinoAlimento_dev#67: incluído tratamento de erro para rota pedidosfornecedores/id

--- app/src/controllers/ComposicaoController.js
+++ app/src/controllers/ComposicaoController.js
@@ -1154,6 +1154,11 @@ module.exports = {
         const cicloId = req.params.id
 
         const dadosCiclo = await Ciclo.getCicloId(cicloId)
+        
+        if (dadosCiclo == 'error') {
+            return res.send('Ciclo não existe!')
+        }
+
         ciclo = dadosCiclo.ciclo[0]
         cicloCestas = dadosCiclo.cicloCestas
 
