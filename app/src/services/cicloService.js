28d812a758db7435f4c7b47239c70f8c334f8662
Alnus Tmp <alnus@disroot.org>
2025-09-16 16:53:17 +0000

    close Tekopora/DivinoAlimento#8

---
+++ app/src/services/cicloService.js
@@ -0,0 +1,23 @@
+const Ciclo = require('../model/Ciclo')
+const PontoEntrega = require('../model/PontoEntrega')
+const Cesta = require('../model/Cesta')
+const Produto = require('../model/Produto')
+const Profile = require('../model/Profile')
+
+class CicloService {
+    async criarCiclo() {
+    const pontosEntrega = await PontoEntrega.get();
+    const tiposCesta = await Cesta.getCestasAtivas();
+
+    await Cesta.verificaCriaCestasInternas();
+
+    const ciclo = {
+      pontosEntrega,
+      tiposCesta,
+    };
+
+    return ciclo;
+  }
+}
+
+module.exports = CicloService;
