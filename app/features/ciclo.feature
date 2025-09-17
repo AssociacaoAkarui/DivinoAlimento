ceb84e043d58d1f9c275cb1a7d7aa44a0f3f2416
Alnus Tmp <alnus@disroot.org>
2025-09-17 15:17:39 +0000

    close Tekopora/DivinoAlimento#14

--- app/features/ciclo.feature
+++ app/features/ciclo.feature
@@ -2,15 +2,17 @@ Funcionalidade: Gestión de Ciclos
 
   Cenário: Criar uma Nova Cesta
     Dado que eu quero criar uma nova Cesta
-    Quando eu crio a Cesta
+    Quando eu crio 1 Cesta
     Então a Cesta deve ser criada corretamente
 
   Cenário: Criar um novo Ponto de Entrega
     Dado que eu quero criar um novo Ponto de Entrega
-    Quando eu crio o Ponto de Entrega
+    Quando eu crio 1 Ponto de Entrega
     Então o Ponto de Entrega deve ser criado corretamente
 
   Cenário: Criar um novo ciclo com pontos de entrega e cestas ativas
-    Dado que o sistema possui um ponto de entrega e 2 cestas ativas
+    Dado que eu quero criar um novo Ciclo
+    Quando eu crio 2 Ponto de Entrega
+    Quando eu crio 2 Cesta
     Quando o usuário cria um novo ciclo
     Então o ciclo deve ser criado com os pontos de entrega e cestas ativas
--- app/features/step_definitions/ciclo.js
+++ app/features/step_definitions/ciclo.js
@@ -1,53 +1,63 @@
-const { Given, When, Then } = require('@cucumber/cucumber');
+const { Given, When, Then, Before } = require('@cucumber/cucumber');
 const { expect } = require('chai');
 
 const { sequelize, Cesta, PontoEntrega } = require('../../models');
 const Factories = require('./support/factories')
 const { CicloService } = require('../../src/services/services');
 
+let novasCestas = [];
+let createdCestas = [];
+let novosPontosEntrega = [];
+let createdPontosEntrega = [];
+let cicloService;
+let ciclo;
+
+
 Given('que eu quero criar uma nova Cesta', function() {
-  novaCesta = Factories.CestaFactory.create();
 });
 
-When('eu crio a Cesta', async function() {
-  createdCesta = await Cesta.create(novaCesta);
+When('eu crio {int} Cesta', async function(numeroDeCestas) {
+  novasCestas = [];
+
+  for (let i = 0; i < numeroDeCestas; i++) {
+      novasCestas.push(Factories.CestaFactory.create());
+  }
+
+  createdCestas = await Promise.all(novasCestas.map(async (novaCesta) => {
+      return await Cesta.create(novaCesta);
+  }));
 });
 
 Then('a Cesta deve ser criada corretamente', function() {
-  expect(createdCesta).to.be.an('object');
-  expect(createdCesta.nome).to.equal(novaCesta.nome);
-  expect(createdCesta.valormaximo).to.equal(novaCesta.valormaximo);
-  expect(createdCesta.status).to.equal(novaCesta.status);
+  expect(createdCestas[0]).to.be.an('object');
+  expect(createdCestas[0].nome).to.equal(novasCestas[0].nome);
+  expect(createdCestas[0].valormaximo).to.equal(novasCestas[0].valormaximo);
+  expect(createdCestas[0].status).to.equal(novasCestas[0].status);
 });
 
 Given('que eu quero criar um novo Ponto de Entrega', function() {
-  novoPontoEntrega = Factories.PontoEntregaFactory.create();
 });
 
-When('eu crio o Ponto de Entrega', async function() {
-  createdPontoEntrega = await PontoEntrega.create(novoPontoEntrega);
+When('eu crio {int} Ponto de Entrega', async function(numeroDePontoEntrega) {
+  novosPontosEntrega = [];
+
+  for (let i = 0; i < numeroDePontoEntrega; i++) {
+      novosPontosEntrega.push(Factories.PontoEntregaFactory.create());
+  }
+
+  createdPontosEntrega = await Promise.all(novosPontosEntrega.map(async (novoPontoEntrega) => {
+      return await PontoEntrega.create(novoPontoEntrega);
+  }));
 });
 
 Then('o Ponto de Entrega deve ser criado corretamente', function() {
-  expect(createdPontoEntrega).to.be.an('object');
-  expect(createdPontoEntrega.nome).to.equal(novoPontoEntrega.nome);
-  expect(createdPontoEntrega.endereco).to.equal(novoPontoEntrega.endereco);
-  expect(createdPontoEntrega.status).to.equal(novoPontoEntrega.status);
+  expect(createdPontosEntrega[0]).to.be.an('object');
+  expect(createdPontosEntrega[0].nome).to.equal(novosPontosEntrega[0].nome);
+  expect(createdPontosEntrega[0].endereco).to.equal(novosPontosEntrega[0].endereco);
+  expect(createdPontosEntrega[0].status).to.equal(novosPontosEntrega[0].status);
 });
 
-Given('que o sistema possui um ponto de entrega e {int} cestas ativas', async function (numeroDeCestas) {
-    novoPontoEntrega = Factories.PontoEntregaFactory.create();
-    createdPontoEntrega = await PontoEntrega.create(novoPontoEntrega);
-
-    const novasCestas = [];
-
-    for (let i = 0; i < numeroDeCestas; i++) {
-        novasCestas.push(Factories.CestaFactory.create());
-    }
-
-    const createdCestas = await Promise.all(novasCestas.map(async (novaCesta) => {
-        return await Cesta.create(novaCesta);
-    }));
+Given('que eu quero criar um novo Ciclo', async function () {
 });
 
 When('o usuário cria um novo ciclo', async function () {
@@ -56,6 +66,6 @@ When('o usuário cria um novo ciclo', async function () {
 });
 
 Then('o ciclo deve ser criado com os pontos de entrega e cestas ativas', function () {
-  expect(ciclo.pontosEntrega).to.have.lengthOf(1);
-  expect(ciclo.tiposCesta).to.have.lengthOf(2);
+  expect(ciclo.pontosEntrega).to.have.lengthOf(createdPontosEntrega.length);
+  expect(ciclo.tiposCesta).to.have.lengthOf(createdCestas.length);
 });
