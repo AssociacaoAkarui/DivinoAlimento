adbe0981669c1fcee2b542a63c0d873820e929be
Alnus Tmp <alnus@disroot.org>
2025-09-17 00:48:15 +0000

    close Tekopora/DivinoAlimento#11

--- app/src/services/services.js
+++ app/src/services/services.js
@@ -4,19 +4,50 @@ const Cesta = require('../model/Cesta')
 const Produto = require('../model/Produto')
 const Profile = require('../model/Profile')
 
+
+class PontoEntregaModel {
+    constructor(dados) {
+    this.id = dados.id;
+    this.nome = dados.nome;
+    this.endereco = dados.endereco;
+    this.status = dados.status;
+  }
+}
+
+class TipoCestaModel {
+  constructor(dados) {
+    this.id = dados.id;
+    this.nomes = dados.nome;
+    this.valormaximo = dados.valormaximo;
+    this.status = dados.status;
+  }
+}
+
+
 class CicloService {
-    async criarCiclo() {
-    const pontosEntrega = await PontoEntrega.get();
-    const tiposCesta = await Cesta.getCestasAtivas();
+  async criarCiclo() {
+    try {
+      const pontosEntrega = await PontoEntrega.get();
+      const tiposCesta = await Cesta.getCestasAtivas();
 
-    await Cesta.verificaCriaCestasInternas();
+      await Cesta.verificaCriaCestasInternas();
 
-    const ciclo = {
-      pontosEntrega,
-      tiposCesta,
-    };
+      return {
+        pontosEntrega: this.criarInstanciasPontoEntrega(pontosEntrega),
+        tiposCesta: this.criarInstanciasTipoCesta(tiposCesta)
+      };
+    } catch (error) {
+      console.error('Erro ao criar ciclo:', error);
+      throw error;
+    }
+  }
 
-    return ciclo;
+  criarInstanciasPontoEntrega(dados) {
+    return dados.map(dado => new PontoEntregaModel(dado));
+  }
+
+  criarInstanciasTipoCesta(dados) {
+    return dados.map(dado => new TipoCestaModel(dado));
   }
 }
 
